52e4c2dcb970b5406002b67aaf3aebb407c1d359
Danny Sheehan <Daniel.sheehan03@gmail.com>
2025-07-07 12:34:35 -0400

    Clean up: remove test and unused setup files for production

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders LG Mobile Detailing', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/LG Mobile Detailing/i);
-  expect(linkElement).toBeInTheDocument();
-});
